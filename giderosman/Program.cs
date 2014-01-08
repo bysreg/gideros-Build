898f6d46f9876d7129850f0e39012d579c1f28d8
bysreg <hilman09121991@gmail.com>
2014-01-08 21:45:21 +0700

    + edit android manifest settings + replace icons automatically + Separate function to separate normal res project and high res project

--- giderosman/Program.cs
+++ giderosman/Program.cs
@@ -4,34 +4,53 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Drawing;
+using System.Diagnostics;
+using System.Xml;
 
 namespace giderosman
 {
     class Program
-    {        
+    {
 
-        static void Main(string[] args)
+        static void Export(string gdrexportPath, string gprojPath, string outputPath)
         {
-            string inputFolderName = "test-folder";
-            string outputFolderName = "export";            
+            Console.WriteLine("Exporting ... ");
 
-            Console.WriteLine("giderosman");
+            if (Directory.Exists(outputPath))
+               Directory.Delete(outputPath, true);
+            Directory.CreateDirectory(outputPath);
 
-            string inputPath = Path.Combine(".", inputFolderName);
-            string outputPath = Path.Combine(".", outputFolderName);
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.CreateNoWindow = true;
+            startInfo.FileName = gdrexportPath;
+            startInfo.Arguments = "-platform android -package \"com.orgcomgames.elementalclash\" -encrypt \"" + gprojPath + "\" \"" + outputPath + "\"";
+            Console.WriteLine(startInfo.Arguments);
+            int exitCode;
+            using (Process proc = Process.Start(startInfo))
+            {
+                proc.WaitForExit();
+                exitCode = proc.ExitCode;
+            }
+
+            Console.WriteLine("Exporting finish : " + exitCode);
+        }
+
+        //membuat salinan yang memisahkan antara 2x dengan normal
+        static void Separate(string inputPath, string outputPath)
+        {
             string normalPath = Path.Combine(outputPath, "normal");
             string hdPath = Path.Combine(outputPath, "2x");
-            if(Directory.Exists(normalPath))
+            
+            if (Directory.Exists(normalPath))
                 Directory.Delete(normalPath, true);
             if (Directory.Exists(hdPath))
-                Directory.Delete(hdPath, true);
-            Directory.CreateDirectory(outputPath);
+                Directory.Delete(hdPath, true);            
             Directory.CreateDirectory(normalPath);
             Directory.CreateDirectory(hdPath);
-            
+
             Stack<string> q = new Stack<string>();
-            Dictionary<string, string> normald = new Dictionary<string, string>();  
-            Dictionary<string, string> highd = new Dictionary<string, string>();            
+            Dictionary<string, string> normald = new Dictionary<string, string>();
+            Dictionary<string, string> highd = new Dictionary<string, string>();
             normald[Path.GetFullPath(inputPath)] = normalPath;
             highd[Path.GetFullPath(inputPath)] = hdPath;
             foreach (var files in Directory.GetFiles(inputPath))
@@ -40,21 +59,21 @@ namespace giderosman
                 q.Push(Path.GetFullPath(folders));
 
             while (q.Count != 0)
-            {                
+            {
                 string path = q.Pop();
-                string parent = Directory.GetParent(path).FullName;                
-                Console.WriteLine("processing " + path);                                                
+                string parent = Directory.GetParent(path).FullName;
+                Console.WriteLine("processing " + path);
                 if (File.Exists(path)) // it's a file
                 {
                     if (!Path.GetFileName(path).Contains("@2x"))
                     {
                         normald[path] = Path.Combine(normald[parent], Path.GetFileName(path));
-                        File.Copy(path, normald[path]);                        
-                        string ext = Path.GetExtension(path);                        
+                        File.Copy(path, normald[path]);
+                        string ext = Path.GetExtension(path);
                         if (ext == ".png" || ext == ".jpg")
                         {
                             string path2x = Path.Combine(parent, Path.GetFileNameWithoutExtension(path) + "@2x" + ext);
-                            if (!File.Exists(path2x)) // jika ada gambar versi yang lebih bagus, jangan kopi file in ke high
+                            if (!File.Exists(path2x)) // jika ada gambar versi yang lebih bagus, jangan kopi file ini ke high
                             {
                                 highd[path] = Path.Combine(highd[parent], Path.GetFileName(path));
                                 File.Copy(path, highd[path]);
@@ -66,10 +85,10 @@ namespace giderosman
                             File.Copy(path, highd[path]);
                         }
                     }
-                    else 
+                    else
                     {
                         highd[path] = Path.Combine(highd[parent], Path.GetFileName(path));
-                        File.Copy(path, highd[path]);                        
+                        File.Copy(path, highd[path]);
                     }
                 }
                 else // it's a dir
@@ -85,7 +104,92 @@ namespace giderosman
                         q.Push(Path.GetFullPath(folders));
                 }
             }
+        }
 
+        static void EditAndroidManifest(string projectPath, string versionCode, string versionName)
+        {
+            XmlDocument doc = new XmlDocument();
+            //search android manifest
+            string[] ampath = Directory.GetFiles(projectPath, "AndroidManifest.xml", SearchOption.AllDirectories);
+            doc.Load(ampath[0]);
+            XmlNamespaceManager nsmgr = new XmlNamespaceManager(doc.NameTable);
+            string strNamespace = doc.DocumentElement.NamespaceURI;
+            nsmgr.AddNamespace("android", strNamespace);
+
+            //set versionCode and versionName 
+            doc.SelectSingleNode("//manifest").Attributes["android:versionCode"].InnerText = versionCode;
+            doc.SelectSingleNode("//manifest").Attributes["android:versionName"].InnerText = versionName;
+            
+            //set installLocation to preferExternal
+            doc.SelectSingleNode("//manifest").Attributes["android:installLocation"].InnerText = "preferExternal";
+            
+            //delete node permission for ACCESS_FINE_LOCATION
+            var locationPermission = doc.SelectSingleNode("//uses-permission[@android:name='android.permission.ACCESS_FINE_LOCATION']", nsmgr);
+            if(locationPermission != null)
+                doc.DocumentElement.RemoveChild(locationPermission);            
+
+            //add node permission for BILLING if it does not exist
+            var checkBilling = doc.SelectSingleNode("//uses-permission[@android:name='com.android.vending.BILLING']", nsmgr);            
+            if (checkBilling == null)
+            {
+                var billingNode = doc.CreateNode(XmlNodeType.Element, "uses-permission", strNamespace);
+                XmlAttribute billingNameAttr = doc.CreateAttribute("android:name");
+                billingNameAttr.Value = "com.android.vending.BILLING";
+                billingNode.Attributes.Append(billingNameAttr);
+                doc.SelectSingleNode("//manifest").InsertAfter(billingNode, doc.SelectSingleNode("//uses-sdk"));
+            }                       
+
+            //set allowBackup to false
+            var allowBackupAttr = doc.CreateAttribute("android:allowBackup");
+            allowBackupAttr.Value = "false";
+            var node = doc.SelectSingleNode("//application").Attributes.Append(allowBackupAttr);
+
+            doc.Save(ampath[0]);
+        }
+
+        static void ReplaceIcons(string projectPath, string iconPath)
+        {
+            //replace icon
+            string hdpiFolderPath = Directory.GetDirectories(projectPath, "drawable-hdpi", SearchOption.AllDirectories)[0];
+            string mdpiFolderPath = Directory.GetDirectories(projectPath, "drawable-mdpi", SearchOption.AllDirectories)[0];
+            string xhdpiFolderPath = Directory.GetDirectories(projectPath, "drawable-xhdpi", SearchOption.AllDirectories)[0];
+            //xxhdpi folder does not always exist
+            var check_exist = Directory.GetDirectories(projectPath, "drawable-xxhdpi", SearchOption.AllDirectories);
+            string xxhdpiFolderPath = null;
+            if (check_exist != null)
+            {
+                xxhdpiFolderPath = Path.Combine(Directory.GetDirectories(projectPath, "res", SearchOption.AllDirectories)[0], "drawable-xxhdpi");
+                Directory.CreateDirectory(xxhdpiFolderPath);
+            }
+            else
+            {
+                xxhdpiFolderPath = Directory.GetDirectories(projectPath, "drawable-xxhdpi", SearchOption.AllDirectories)[0];
+            }
+                
+            File.Copy(Path.Combine(iconPath, "hdpi", "icon.png"), Path.Combine(hdpiFolderPath, "icon.png"), true);
+            File.Copy(Path.Combine(iconPath, "mdpi", "icon.png"), Path.Combine(mdpiFolderPath, "icon.png"), true);
+            File.Copy(Path.Combine(iconPath, "xhdpi", "icon.png"), Path.Combine(xhdpiFolderPath, "icon.png"), true);
+            File.Copy(Path.Combine(iconPath, "xxhdpi", "icon.png"), Path.Combine(xxhdpiFolderPath, "icon.png"), true);            
+        }
+
+        static void Main(string[] args)
+        {            
+            string gdrexportPath = @"C:\Program Files (x86)\Gideros\Tools\gdrexport.exe";
+            string gprojPath = @"C:\Users\Asus\Desktop\Elemental Clash\EC - 2\trunk\Project\ElementalClash\ElementalClash.gproj";
+            string versionCode = "5";
+            string versionName = "1.0.4";
+            string iconPath = @"C:\Users\Asus\Desktop\Elemental Clash\icons";
+                              
+            string origPath = Path.GetFullPath(".\\orig");
+
+            Console.WriteLine("giderosman");
+
+            Export(gdrexportPath, gprojPath, origPath);            
+            EditAndroidManifest(origPath, versionCode, versionName);
+            ReplaceIcons(origPath, iconPath);
+            Separate(origPath, ".");
+
+            Console.WriteLine("DONE! press enter to exit");
 #if DEBUG
             Console.Read();
 #endif
